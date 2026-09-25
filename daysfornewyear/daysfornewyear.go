7ca20e0237d41f3db8c9f6affe96fb525a3a543b
so1ove1 <159862767+so1ove1@users.noreply.github.com>
2026-09-25 15:08:06 +0800

    Update daysfornewyear.go

--- daysfornewyear/daysfornewyear.go
+++ daysfornewyear/daysfornewyear.go
@@ -2,6 +2,8 @@ package daysfornewyear
 
 import "time"
 
+// DaysUntilNewYear возвращает количество дней от переданной даты 
+// до 1 января следующего календраного года (следующего Нового года)
 func DaysUntilNewYear(t time.Time) int {
 	today := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 	target := time.Date(today.Year()+1, time.January, 1, 0, 0, 0, 0, today.Location())
