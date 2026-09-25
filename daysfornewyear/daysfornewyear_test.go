0610f1afb8150df2972d3113a430f719a0c87bbe
so1ove1 <159862767+so1ove1@users.noreply.github.com>
2026-09-25 14:47:10 +0800

    Update daysfornewyear_test.go

--- daysfornewyear/daysfornewyear_test.go
+++ daysfornewyear/daysfornewyear_test.go
@@ -1,6 +1,6 @@
 package daysfornewyear
 
-import(
+import (
 	"testing"
 	"time"
 )
@@ -12,7 +12,7 @@ func TestDaysUntilNewYear(t *testing.T) {
 		want  int
 	}{
 		{"начало года", time.Date(2026, time.January, 1, 0, 0, 0, 0, time.UTC), 365},
-		{"конец года",  time.Date(2026, time.December, 31, 0, 0, 0, 0, time.UTC), 1},
+		{"конец года", time.Date(2026, time.December, 31, 0, 0, 0, 0, time.UTC), 1},
 		{"високосный год", time.Date(2024, time.January, 1, 0, 0, 0, 0, time.UTC), 366},
 		{"1 марта не високосного", time.Date(2026, time.March, 1, 0, 0, 0, 0, time.UTC), 306},
 		{"1 марта високосного", time.Date(2024, time.March, 1, 0, 0, 0, 0, time.UTC), 306},
